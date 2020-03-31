16c44f83fbf9cf77979ce580718f0861ec44797f
mohitkumartoshniwal <mohitkrtoshniwal.com>
2020-03-31 13:58:58 +0530

    configured prettier and eslint

--- src/App.js
+++ src/App.js
@@ -5,44 +5,31 @@
 //         {},
 //         React.createElement("h1", {}, props.name),
 //         React.createElement("h2", {}, props.animal)
-        
+
 //     )
 
 // }
-const Pet = ({name,animal})=> {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal)
-        
-    )
-
-}
+const Pet = ({ name, animal }) => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal)
+  );
+};
 const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "adopt me"),
+    React.createElement(Pet, {
+      name: "tom",
+      animal: "cat",
+    }),
 
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "adopt me"),
-            React.createElement(Pet, 
-                { 
-                    name: "tom", 
-                    animal: "cat" 
-                }),
+    React.createElement(Pet, {
+      name: "jerry",
+      animal: "mouse",
+    }),
+  ]);
+};
 
-                React.createElement(Pet, 
-                    { 
-                        name: "jerry", 
-                        animal: "mouse" 
-                    }),
-        ]
-
-    )
-}
-
-ReactDOM.render(
-    React.createElement(App), document.getElementById("root")
-
-)
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
