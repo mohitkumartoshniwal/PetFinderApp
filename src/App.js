99a148976e16f19dd6f25e6014dada291c33283d
mohitkumartoshniwal <mohitkrtoshniwal.com>
2020-04-01 04:01:58 +0530

    used useState, useEffect , made  custome hooks and increased abstraction, update ESLint, made api call to petfinder api,  async calls to Petfinder AP

--- src/App.js
+++ src/App.js
@@ -13,20 +13,39 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "adopt me"),
-    React.createElement(Pet, {
-      name: "tom",
-      animal: "cat",
-    }),
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, "adopt me"),
+  //     React.createElement(Pet, {
+  //       name: "tom",
+  //       animal: "cat",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "jerry",
+  //       animal: "mouse",
+  //     }),
+  //   ]);
 
-    React.createElement(Pet, {
-      name: "jerry",
-      animal: "mouse",
-    }),
-  ]);
+  //   return (
+  //     <div>
+  //       <h1 id="siefn">adopt me</h1>
+  //       <Pet name="tom" animal="cat" />
+  //       <Pet name="jerry" animal="mouse" />
+  //     </div>
+  //   );
+
+  //React.StrictMode>//for additional warnings in deve env
+  return (
+    <React.StrictMode>
+      <div>
+        <h1 id="impo">adopt me</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+//render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
